fdff0c07ac38aafb7340023bf3be62b405fc7034
Aku52 <kotik.52@tutamail.com>
2025-04-22 20:10:14 +0300

    sposob_2

---
+++ main.py
@@ -0,0 +1,7 @@
+import operacui as m1
+
+def main():
+    m1.main()
+
+if __name__=="__main__":
+    main()
---
+++ operacui.py
@@ -0,0 +1,42 @@
+# Aku
+
+import pandas as pd
+
+class Processing:
+
+    def __init__(self):
+        pass
+
+    # Oбработка и фильтрация данных файла
+    def obrabotka (self):
+       
+        # Читаем файл
+        df = pd.read_csv('var2.csv')
+        
+        # Разделение датасета 
+        filtered1_df = df[df['Тип операции'] == 'списание средств']
+        filtered2_df = df[df['Тип операции'] != 'списание средств']
+        
+        # Создание отдельных файлов 
+        filtered1_df.to_csv('sredstva_1.csv', index= False, encoding='utf-8')
+        filtered2_df.to_csv('sredstva_2.csv', index= False, encoding='utf-8')
+        
+        # Создание полиморфизма унарного оператора для удаления дубликатов
+        num_duplicates = df.duplicated().sum()
+        df = df.drop_duplicates()
+        # Bывод количества удаленных дубликатов
+        print('Количество повторяющихся строк в наборе:', num_duplicates)
+
+
+def main():
+    pay = Processing()
+    pay.obrabotka()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
